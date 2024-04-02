7c42d33cf49326b336967c214bfd383060e0eeac
nicosimone23 <162124898+nicosimone23@users.noreply.github.com>
2024-04-02 14:37:48 +0200

    q

---
+++ Script/script.js
@@ -0,0 +1,7 @@
+function doLogin(e) {
+    var username = document.getElementById ("username") . value;
+    var password = document.getElementById ("password") . value;
+    if (username== ""; password=="") {
+    alert ("Username e password sono obbligatori") ;
+    }
+    }
