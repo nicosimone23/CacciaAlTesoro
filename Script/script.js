e9cad042776a7a68aba29fe2e601e7171d393052
nicosimone23 <162124898+nicosimone23@users.noreply.github.com>
2024-05-13 08:10:23 +0200

    Update script.js

--- Script/script.js
+++ Script/script.js
@@ -1,26 +1,27 @@
-function doLogin() {
-    var username = document.getElementById("username").value;
-    var password = document.getElementById("password").value;
+function doLogin(event) {
+  var username = document.getElementById("username").value;
+  var password = document.getElementById("password").value;
   
-    if (username === "" || password === "") {
-      alert("Username e password sono obbligatori");
-    } else {
-      // chiamiamo il server per effettuare la verifica
-      fetch('https://www.cacciaapi.altervista.org/login.php/loginIpad', {
-        method: 'POST', // o 'GET'
-        headers: {
-          'Content-Type': 'application/json',
-        },
-        body: JSON.stringify({
-          "username": username,
-          "password": password
-        })
+  if (username === "" || password === "") {
+    alert("Username and password are mandatory.");
+  } else {
+    fetch("https://www.cacciaapi.altervista.org/login.php/loginIpad", {
+      method: "POST",
+      headers: {
+        "Content-Type": "application/json"
+      },
+      body: JSON.stringify({
+        "username": username,
+        "password": password
       })
-      .then(response => response.json()) // converte la risposta in JSON
-      .then(data => console.log(data)) // stampa i dati
-      .catch(error => {
-        console.error('Errore:', error);
-      });
-    }
+    })
+    .then(response => response.json())
+    .then(data => {
+      console.log(data);
+      window.location.href = "game.html";
+    })
+    .catch(error => {
+      console.error("Error:", error);
+    });
   }
-  
+}
